38109bf0bcdc674cfd15fac94a2cdaf5090dc9d5
Felipe Cerero <luis.cerero@itglobers.com>
2025-08-28 08:11:18 -0500

    fix: redirect login

--- react/components/order-placed/OrderPlacedHeader.tsx
+++ react/components/order-placed/OrderPlacedHeader.tsx
@@ -44,87 +44,87 @@ const OrderPlacedHeader = () => {
     description = "No te pierdas nuestras últimas ofertas y novedades. ";
   }
 
-// ------------------------------------------- DataLayer OrderPlaced  ------------------------------------------- //
+  // ------------------------------------------- DataLayer OrderPlaced  ------------------------------------------- //
 
-useEffect(() => {
-  if (!order || !order.paymentData.transactions[0]?.transactionId) return // Verifica si `order` está disponible antes de continuar
+  useEffect(() => {
+    if (!order || !order.paymentData.transactions[0]?.transactionId) return // Verifica si `order` está disponible antes de continuar
 
-  // Función para encriptar los datos con SHA-256
-  const hashData = async (data: string) => {
-    if (!data) return '' // Evita errores con valores undefined o null
-    const encoder = new TextEncoder()
-    const dataBuffer = encoder.encode(data)
-    const hashBuffer = await crypto.subtle.digest('SHA-256', dataBuffer)
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('')
-  }
-
-  // Obtener los datos del usuario y encriptarlos
-  const getUserData = async () => {
-    const userData = {
-      nombre: order.clientProfileData?.firstName || '',
-      apellido: order.clientProfileData?.lastName || '',
-      celular: order.clientProfileData?.phone || '',
-      correo: order.clientProfileData?.email || '',
-      ciudad: order.deliveryParcels?.[0]?.address?.city || '',
+    // Función para encriptar los datos con SHA-256
+    const hashData = async (data: string) => {
+      if (!data) return '' // Evita errores con valores undefined o null
+      const encoder = new TextEncoder()
+      const dataBuffer = encoder.encode(data)
+      const hashBuffer = await crypto.subtle.digest('SHA-256', dataBuffer)
+      const hashArray = Array.from(new Uint8Array(hashBuffer))
+      return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('')
     }
 
-    return {
-      nombre: await hashData(userData.nombre),
-      apellido: await hashData(userData.apellido),
-      celular: await hashData(userData.celular),
-      correo: await hashData(userData.correo),
-      ciudad: await hashData(userData.ciudad),
+    // Obtener los datos del usuario y encriptarlos
+    const getUserData = async () => {
+      const userData = {
+        nombre: order.clientProfileData?.firstName || '',
+        apellido: order.clientProfileData?.lastName || '',
+        celular: order.clientProfileData?.phone || '',
+        correo: order.clientProfileData?.email || '',
+        ciudad: order.deliveryParcels?.[0]?.address?.city || '',
+      }
+
+      return {
+        nombre: await hashData(userData.nombre),
+        apellido: await hashData(userData.apellido),
+        celular: await hashData(userData.celular),
+        correo: await hashData(userData.correo),
+        ciudad: await hashData(userData.ciudad),
+      }
     }
-  }
 
-  // Verifica si la dataLayer está disponible
-  if (window.dataLayer) {
-    const items = order.items.map((item: any) => ({
-      item_id: item.id,
-      item_name: item.name,
-      coupon: item.coupon || '',
-      discount: (item.discount || 0) / 100,
-      affiliation: 'Saba',
-      item_brand: item.brand,
-      item_category: item.category,
-      item_variant: item.variant,
-      price: item.price / 100, // Asegura que esté en formato correcto
-      currency: 'MXN',
-      quantity: item.quantity,
-    }))
+    // Verifica si la dataLayer está disponible
+    if (window.dataLayer) {
+      const items = order.items.map((item: any) => ({
+        item_id: item.id,
+        item_name: item.name,
+        coupon: item.coupon || '',
+        discount: (item.discount || 0) / 100,
+        affiliation: 'Saba',
+        item_brand: item.brand,
+        item_category: item.category,
+        item_variant: item.variant,
+        price: item.price / 100, // Asegura que esté en formato correcto
+        currency: 'MXN',
+        quantity: item.quantity,
+      }))
 
-    const formatCurrency = (value: number) =>
-      new Intl.NumberFormat('es-MX', {
-        style: 'currency',
-        currency: 'MXN'
-      }).format(value / 100)
+      const formatCurrency = (value: number) =>
+        new Intl.NumberFormat('es-MX', {
+          style: 'currency',
+          currency: 'MXN'
+        }).format(value / 100)
 
-    // Obtén los datos encriptados del usuario y empuja al dataLayer
-    getUserData().then(encryptedUserData => {
-      window.dataLayer = window.dataLayer || []
-      window.dataLayer.push({
-        event: 'purchase',
-        ecommerce: {
-          affiliation: 'Online store',
-          coupon: '',
-          currency: 'MXN',
-          items,
-          flow_type:
-            order.paymentData?.transactions[0]?.payments[0]?.paymentSystemName === 'Pago en Tienda'
-              ? 'apartado'
-              : 'compra',
-          transaction_id: order.paymentData.transactions[0]?.transactionId,
-          value: order.value / 100,
-          tax: order.totals?.find((t: any) => t.id === 'Tax')?.value / 100 || 0,
-          shipping: formatCurrency(order.deliveryParcels?.[0]?.price || 0),
-          user_data: encryptedUserData,
-          log_status: order.clientProfileData?.isAuthenticated ? 'Yes' : 'No',
-        },
+      // Obtén los datos encriptados del usuario y empuja al dataLayer
+      getUserData().then(encryptedUserData => {
+        window.dataLayer = window.dataLayer || []
+        window.dataLayer.push({
+          event: 'purchase',
+          ecommerce: {
+            affiliation: 'Online store',
+            coupon: '',
+            currency: 'MXN',
+            items,
+            flow_type:
+              order.paymentData?.transactions[0]?.payments[0]?.paymentSystemName === 'Pago en Tienda'
+                ? 'apartado'
+                : 'compra',
+            transaction_id: order.paymentData.transactions[0]?.transactionId,
+            value: order.value / 100,
+            tax: order.totals?.find((t: any) => t.id === 'Tax')?.value / 100 || 0,
+            shipping: formatCurrency(order.deliveryParcels?.[0]?.price || 0),
+            user_data: encryptedUserData,
+            log_status: order.clientProfileData?.isAuthenticated ? 'Yes' : 'No',
+          },
+        })
       })
-    })
-  }
-}, [order])
+    }
+  }, [order])
 
   return (
     <div className={handles['order-placed-header']}>
@@ -144,7 +144,7 @@ useEffect(() => {
         </div>
         {paymentMethod != 'Pago en Tienda' ? (
           <div className={handles['order-placed-header__confirmations-button']}>
-            <a className={handles['order-placed-header__confirmations-link']} href='/login'>
+            <a className={handles['order-placed-header__confirmations-link']} href='https://www.intimawear.saba.com.mx/login'>
               Ver estatus de mi pedido
             </a>
           </div>
